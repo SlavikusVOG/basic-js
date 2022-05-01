5a741ba7b672c72d0c0f045cb24ef5ec5324a25b
Viacheslav Olishevko <volishevko@xbsoftware.by>
2022-05-01 11:46:46 +0300

    fix: fix date validation

--- src/what-season.js
+++ src/what-season.js
@@ -11,19 +11,14 @@ const { CONSTANTS } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function checkForInvalid(date) {
-	if (date instanceof Date) {
-		return true;
-	}
-	return new Date(date) instanceof Date ? true : false;
-}
 
 function getSeason(date) {
 	const [winter, spring, summer, autumn] = ["winter", "spring", "summer", "autumn"];
 	if (!date) {
 		return 'Unable to determine the time of year!'
 	}
-	if (!checkForInvalid(date)) {
+	if (date instanceof Date === false ||
+		Object.getOwnPropertyNames(date).length > 0) {
 		throw new Error('Invalid date!');
 	}
 	let season;
@@ -55,22 +50,6 @@ function getSeason(date) {
 	return season;
 }
 
-const [
-	winter,
-	spring,
-	summer,
-	autumn,
-] = [
-		new Date(2019, 11, 22, 23, 45, 11, 500),
-		new Date(2018, 4, 17, 11, 27, 4, 321),
-		new Date(2017, 6, 11, 23, 45, 11, 500),
-		new Date(1994, 8, 26, 3, 0, 11, 500),
-	];
-getSeason(winter);
-getSeason(spring);
-getSeason(summer);
-getSeason(autumn);
-
 module.exports = {
   getSeason
-};
+}
